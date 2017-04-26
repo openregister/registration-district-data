40b57349f94c3cb1b0592caca39cd4430c65da06
Paul Downey <psd@whatfettle.com>
2017-04-26 12:19:31 +0100

    More illegal quoting

--- lists/gro-district-book/squash.sh
+++ lists/gro-district-book/squash.sh
@@ -4,7 +4,9 @@ sed -e '/^ *$/d' \
   -e 's/ *$//' \
   -e '/^[^ ]/d' \
   -e 's/^ *//' \
-  -e 's/North "/North/' \
+  -e 's/ *& North[" ]*)/\& North)  /' \
+  -e 's/Vale of _ */Vale of/' \
+  -e 's/"//g' \
   -e 's/   */\t/g' \
   -e '/GRO Registration District Book/d' \
   -e '/Page [0-9]* of [0-9]*$/d' \
