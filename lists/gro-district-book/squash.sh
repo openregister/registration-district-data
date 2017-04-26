b630de2f21655384a815b205319e33309e889ea4
Paul Downey <psd@whatfettle.com>
2017-04-26 10:53:44 +0100

    Illegal quoting

--- lists/gro-district-book/squash.sh
+++ lists/gro-district-book/squash.sh
@@ -4,6 +4,8 @@ sed -e '/^ *$/d' \
   -e 's/ *$//' \
   -e '/^[^ ]/d' \
   -e 's/^ *//' \
+  -e 's/North "/North/' \
+  -e 's/   */\t/g' \
   -e '/GRO Registration District Book/d' \
   -e '/Page [0-9]* of [0-9]*$/d' \
   -e '/^Key: *District where Records sent:/Q' \
