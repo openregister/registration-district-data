03122bff7ffe2f205a355bb7de883da56581d211
Paul Downey <psd@whatfettle.com>
2017-04-27 10:36:51 +0100

    Normalise underscores in names

--- lists/gro-district-book/squash.sh
+++ lists/gro-district-book/squash.sh
@@ -4,9 +4,12 @@ sed -e '/^ *$/d' \
   -e 's/ *$//' \
   -e '/^[^ ]/d' \
   -e 's/^ *//' \
-  -e 's/ *& North[" ]*)/\& North)  /' \
-  -e 's/Vale of _ */Vale of/' \
   -e 's/"//g' \
+  -e 's/__*/_/g' \
+  -e 's/( */(/g' \
+  -e 's/ *)/)/g' \
+  -e 's/(--*/(_/g' \
+  -e 's/--*)/_)/g' \
   -e '/GRO Registration District Book/d' \
   -e '/Page [0-9]* of [0-9]*$/d' \
   -e '/^Key: *District where Records sent:/Q' \
