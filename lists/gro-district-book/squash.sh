b6a65d7100351708a56aa7513f3f39ee1a5fb454
Paul Downey <psd@whatfettle.com>
2017-04-27 22:02:22 +0100

    Count and fix columns

--- lists/gro-district-book/squash.sh
+++ lists/gro-district-book/squash.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-sed -e '/^ *$/d' \
+sed \
+  -e '/^ *$/d' \
   -e 's/ *$//' \
   -e '/^[^ ]/d' \
   -e 's/^ *//' \
@@ -13,4 +14,30 @@ sed -e '/^ *$/d' \
   -e '/GRO Registration District Book/d' \
   -e '/Page [0-9]* of [0-9]*$/d' \
   -e '/^Key: *District where Records sent:/Q' \
-  -e 's/   */\t/g'
+  -e 's/   */\t/g' \
+  |
+
+  # data specific fixups
+  sed \
+  -e '/^Crawley/s/Crawley/Crawley		/' \
+  -e '/^Crawley/s/18	/18		/' \
+  -e '/Vale of Glamorgan/s/11A/11A				/' \
+  -e '/^Leamington/s/	/		/' \
+  -e '/^North Shields/s/	/					/' \
+  |
+
+  # remove duplicate title rows
+  awk -F'	' '
+  (NR == 1) { print }
+  (NR > 1) { if ($1 !~ "Registration District") print }' \
+  |
+
+  # check number of columns
+  awk -F'	' '
+  (NR == 1) { print; ncols = NF }
+  (NR > 1) {
+      print
+      if (NF != ncols) {
+          printf "error: line %d (%s) has %d cols, expected %d\n", NR, $1, NF, ncols > "/dev/stderr"
+      }
+  }'
