2f0ad3311f18de93f0abf2a61df4a01b0b2a2d3a
Paul Downey <psd@whatfettle.com>
2017-05-04 12:20:14 +0100

    Separate out abolished codes

--- lists/gro-district-book/map.py
+++ lists/gro-district-book/map.py
@@ -8,14 +8,43 @@ import re
 
 sep = '\t'
 
-years = [ '1837', '1852', '1946', '1965', '1974', '1993', '1997' ]
+fields = [
+    'registration-district',
+    'name',
+    'start-date',
+    'end-date',
+]
 
-fields = [ 'registration-district', 'name', 'start-date', 'end-date' ]
+maps = {
+    '1837': 'Sept 1837 to 1851',
+    '1852': '1852 to June 1946',
+    '1946': 'Sept 1946 to March 1965',
+    '1965': 'June 1965 to March 1974',
+    '1974': 'June 1974 to Dec 1992',
+    '1993': 'Mar 1993 to Dec 1996',
+    '1997': '1997 to 2001'
+}
+
+notes = ['abolished']
+
+fieldnames = fields + sorted(list(maps.keys())) + notes
 
 rows = []
 
+def n7e(code):
+    if code.startswith("CREATED") or code.startswith("ABOLISHED"):
+        return ""
+
+    if code.startswith("*"):
+        return ""
+
+    return code
+
+
 def find_code(row):
-    return row['1997 to 2001']
+    code = row['1997 to 2001']
+
+    return n7e(code)
 
 
 def find_date(row, prefix):
@@ -25,15 +54,24 @@ def find_date(row, prefix):
     return ""
 
 
+def abolished(row):
+    note = row['1997 to 2001']
+    if not note.startswith('*'):
+        return ""
+
+    return note.replace("*", "")
+
 def natural_key(key):
     if (key.startswith("*")):
         return int("0" + re.sub("\D", "", key))
     else:
         return int("10000" + re.sub("\D", "", key))
 
-print(sep.join(fields))
+print(sep.join(fieldnames))
 
-for row in csv.DictReader(sys.stdin, delimiter=sep):
+reader = csv.DictReader(sys.stdin, delimiter=sep)
+
+for row in reader:
 
     code = find_code(row)
 
@@ -41,9 +79,12 @@ for row in csv.DictReader(sys.stdin, delimiter=sep):
     row['name'] = row['Registration District']
     row['start-date'] = find_date(row, 'CREATED')
     row['end-date'] = find_date(row, 'ABOLISHED')
+    row['abolished'] = abolished(row)
+
+    for m in maps:
+        row[m] = n7e(row[maps[m]])
 
     rows.append(row)
 
-
 for row in sorted(rows, key=lambda row: natural_key(row['registration-district'])):
-    print(sep.join([row[field] for field in fields]))
+    print(sep.join([row[field] for field in fieldnames]))
